b37b697b1dfe254030c833a68e5888ee04c04b45
PrynsTag <princevelasco16@gmail.com>
2021-05-21 22:18:51 +0800

    Display values in UI for UISlider

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var height: UILabel!
+    @IBOutlet weak var weight: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
-}
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        height.text = "\(String(format: "%.2f", sender.value))m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weight.text = "\(String(format: "%.0f", sender.value))Kg"
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        print(weight / pow(height, 2))
+    }
+}	
